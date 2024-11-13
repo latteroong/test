48e265603e13c0882717890e2835d72dfcee9bce
임예은 <122524095+latteroong@users.noreply.github.com>
2024-11-13 10:53:12 +0900

    0.5 prime number

--- week12.py
+++ week12.py
@@ -1,12 +1,14 @@
-counts = 0
+is_prime = True # count(int)를 is_prime(bool)로 변경
 number = int(input("Input number : "))
-
+if number <= 1:
+    is_prime = False
+    
 for i in range(2, number):
     if number % i == 0:
-        counts = counts + 1
+        is_prime = False    # + 제거
     print(i, end=' ')
 
-if counts == 0:
+if is_prime:    # 비교연산 제거
     print(f"\n{number} is prime number~")
 else:
     print(f"\n{number} is NOT prime number! (divisors : {counts})")
